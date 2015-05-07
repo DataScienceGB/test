de9c6bd3f091f5ba354dca9130b1eccc89610c8b
DataScienceGB <gabriel.banuelos@infomedia.com.mx>
2015-05-06 19:54:13 -0500

    plot 2 ok

--- plot1.R
+++ plot1.R
@@ -1,12 +1,13 @@
-#load data
 library(sqldf)
 
-pc <- read.csv2.sql("./data/power_comsumption_feb2007.txt", sql = 'select * from file where Date in ("1/2/2007","2/2/2007")')
+#load data
+pc <- read.csv2.sql("./data/power_comsumption.txt", sql = 'select * from file where Date in ("1/2/2007","2/2/2007")')
 
 #plot histogram
-png(file="plot1.png")
+#use red color
+
+png(file="plot1.png" ,width = 480, height = 480)
 hist(pc$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)" ,col="red")
-#dev.copy(png,file="plot1.png")
 dev.off()
 
 
--- plot3.R
+++ plot3.R
@@ -4,12 +4,15 @@ library(sqldf)
 
 pc <- read.csv2.sql("./data/power_comsumption.txt", sql = 'select * from file where Date in ("1/2/2007","2/2/2007")')
 
+#add a date-time varialbe to DF 
 datetime<-paste(pc$Date,pc$Time)
 pc$Datetime<-strptime(datetime,"%d/%m/%Y %H:%M:%S")
 
 
 #plot histogram
-png(file="plot2.png",width = 480, height = 480)
-plot(pc$Datetime,pc$Global_active_power,ylab="Global Active Power (kilowatts)",xlab="",type="l")
-dev.off()
+#png(file="plot3.png",width = 480, height = 480)
+
+plot(pc$Datetime,pc$sub_metering_1,ylab="Energy sub metering", xlab="",type="l")
+
+#dev.off()
 
