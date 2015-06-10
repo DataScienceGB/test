c62854471eced3cbbb1f45d368c06c2e003a0d99
DataScienceGB <gabriel.banuelos@infomedia.com.mx>
2015-06-10 17:54:49 -0500

    q4

--- r5_plot3.R
+++ r5_plot3.R
@@ -6,12 +6,20 @@ actdf=read.csv("./activity.csv",stringsAsFactors=F)
 #average days by interval
 act_avg=sqldf("select  interval,avg(steps) as Steps from actdf where steps!='NA' group by interval")
 
-#convert to Date type
-#act_grp$date=paste(act_grp$date,"00:00:00")
-#act_grp$date=strptime(act_grp$date,"%Y-%m-%d %H:%M:%S")
+#create a new data set with to fill missing values
+actdf_imputed=actdf
 
-#Create  create timeline
-plot(act_avg$interval,act_avg$Steps,type="l",col="blue",main="Average number of steps by interval", ylab="Average steps taken by interval",xlab="5-minute day interval")
+#impute NA values using average steps by interval
+#actdf1$steps[isna]=act_avg$Steps
+#Select those rows having na values with is.na and assign them the averege value of its interval
+actdf_imputed$steps[is.na(actdf$steps)]=act_avg$Steps
 
-#Calculate maximum 5-minute interval across all days
-sqldf("select interval, max(Steps) from act_avg group by interval order by 2 desc limit 1")
+#summarize steps by day
+act_grp=sqldf("select date, sum(steps) as Steps from actdf_imputed group by date")
+
+#Create Steps histogram
+hist(act_grp$Steps,col="red",main="Total number of steps taken per day", xlab="Steps taken a day")
+
+#Calculate Mean and Median steps taken per day
+print(c(mean(act_grp$Steps),median(act_grp$Steps)))
+
