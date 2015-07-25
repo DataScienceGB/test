f71c379cc68511454eb049d52c33a74dfeaad091
DataScienceGB <gabriel.banuelos@infomedia.com.mx>
2015-07-24 22:43:32 -0500

    central limit

--- plot1.R
+++ plot1.R
@@ -1,13 +1,3 @@
-library(sqldf)
-
-#load data
-pc <- read.csv2.sql("./data/household_power_consumption.txt", sql = 'select * from file where Date in ("1/2/2007","2/2/2007")')
-
-#plot histogram
-#use red color
-
-png(file="plot1.png" ,width = 480, height = 480)
-hist(pc$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)" ,col="red")
-dev.off()
-
-
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
