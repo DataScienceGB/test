bc5a1524329acd0f6e644e64e0a61a7ce850d61b
DataScienceGB <gabriel.banuelos@infomedia.com.mx>
2015-07-24 22:42:38 -0500

    exponential

--- exp_sim.R
+++ exp_sim.R
@@ -2,21 +2,24 @@ nosim <- 1000
 lambda=.20
 mn=1/lambda
 std=1/lambda
-n=40
-stderr=std/sqrt(n)
+##n=40
+##stderr=std/sqrt(n)
+set.seed(79)
 
-#cfunc <- function(x, n) sqrt(n) * (mean(x) - 3.5) / 1.71
-cfunc <- function(x, n) sqrt(n) * (mean(rexp(n,lambda)) - mn) /std
+
+cfunc <- function(x, n1) sqrt(n1) * (mean(rexp(n1,lambda)) - mn) /std
 dat <- data.frame(
-  x = c(apply(matrix(sample(rexp(n,lambda), nosim * 20, replace = TRUE), 
+  x = c(apply(matrix(sample(rexp(n,lambda), nosim * 5, replace = TRUE),
                      nosim), 1, cfunc, 10),
-        apply(matrix(sample(rexp(n,lambda), nosim * 30, replace = TRUE), 
+        apply(matrix(sample(rexp(n,lambda), nosim * 15, replace = TRUE),
                      nosim), 1, cfunc, 20),
-        apply(matrix(sample(rexp(n,lambda), nosim * 40, replace = TRUE), 
+        apply(matrix(sample(rexp(n,lambda), nosim * 40, replace = TRUE),
                      nosim), 1, cfunc, 30)
         ),
-  size = factor(rep(c(20, 30, 40), rep(nosim, 3))))
-g <- ggplot(dat, aes(x = x, fill = size)) + geom_histogram(alpha = .20, binwidth=.3, colour = "black", aes(y = ..density..)) 
-g <- g + stat_function(fun = dnorm, size = 2)
-#g <- g + stat_function(fun = dexp, size = 2)
+  size = factor(rep(c(5, 15, 40), rep(nosim, 3))))
+g <- ggplot(dat, 
+        aes(x = x, fill = size)) + 
+        geom_histogram(alpha = .20, binwidth=.3, colour = "blue", 
+                        aes(y = ..density..))
+g <- g + stat_function(fun = dnorm, size = 1, colour="red")
 print (g + facet_grid(. ~ size))
